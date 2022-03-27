dcdbee031cfa11058138d43eb493fa48b2dafd0f
Krishna K G <ashwin.krishna.k.g@accenture.com>
2022-03-27 22:19:54 +0530

    changes committed

--- demo.py
+++ demo.py
@@ -5,6 +5,6 @@ print("checking this by adding extra code")
 print("hey ashwin -working in azure devops")
 
 print("hey ashwin -working in azure devops")
-print("editing the code in git repo")
+print("editing the code in git repo i have some changes too")
 
 
